3e53e7d692583509aa2a37e26fa2b44a613812d3
Nishika Gorla <nishika.gorla@gmail.com>
2025-04-23 16:30:04 -0400

    fixed import routes

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// commented out import routes bc database is not connecting w them
-//const bathroomRoutes = require('./routes/bathrooms');
-//app.use('/api/bathrooms', bathroomRoutes);
 
-//const ratingRoutes = require('./routes/rating');
-//app.use('/api/ratings', ratingRoutes);
+const bathroomRoutes = require('./server/routes/bathrooms');
+app.use('/api/bathrooms', bathroomRoutes);
+
+const ratingRoutes = require('./server/routes/rating');
+app.use('/api/ratings', ratingRoutes);
 
 // Connect to MongoDB using the new URI from .env
 mongoose.connect(process.env.MONGODB_URI, {
