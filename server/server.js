55954814bab587f2f27708c875d586954efb04ef
mai tran <maitran@10-19-0-94.dynapool.wireless.nyu.edu>
2025-12-17 13:32:39 -0500

    Fix dotenv config to explicitly load .env file from server directory

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 // ===== CHECK ENV =====
 if (!process.env.MONGODB_URI) {
